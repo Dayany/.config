29a6ea758693a84b9ae048d61b9eb7e555dfe50e
Dayan <yamindayan@gmail.com>
2024-09-02 00:27:39 -0400

    update configurations

---
+++ nvim/lua/plugins/shade.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "miversen33/sunglasses.nvim",
+    config = function()
+      require("sunglasses").setup({
+        filter_type = "SHADE",
+        filter_percent = 0.4,
+      })
+    end,
+  },
+}
